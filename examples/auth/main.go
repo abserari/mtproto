eea86252c5af5158e0d7ae7288581426da444239
AbserAri <abserari@gmail.com>
2021-01-06 19:20:50 +0800

    update for my example

--- examples/auth/main.go
+++ examples/auth/main.go
@@ -24,11 +24,11 @@ func main() {
 	// edit these params for you!
 	client, err := telegram.NewClient(telegram.ClientConfig{
 		// where to store session configuration. must be set
-		SessionFile: "/home/me/.local/var/lib/mtproto/session1.json",
+		SessionFile: "./session1.json",
 		// host address of mtproto server. Actually, it can'be mtproxy, not only official
 		ServerHost: "149.154.167.50:443",
 		// public keys file is patrh to file with public keys, which you must get from https://my.telelgram.org
-		PublicKeysFile: "/home/me/.local/var/lib/mtproto/tg_public_keys.pem",
+		PublicKeysFile: "./keys.pem",
 		AppID:          94575,                              // app id, could be find at https://my.telegram.org
 		AppHash:        "a3406de8d171bb422bb6ddf3bbd800e2", // app hash, could be find at https://my.telegram.org
 		ProxyDialer: telegram.NewSOCK5ProxyDialer("tcp",
@@ -38,14 +38,12 @@ func main() {
 				Password: "wFKo1z8xOr",
 			}),
 	})
-	// dry.PanicIfErr(err)
+	dry.PanicIfErr(err)
 
-	fmt.Println(client.HelpGetConfig())
-	fmt.Println("sdafds")
 	setCode, err := client.AuthSendCode(&telegram.AuthSendCodeParams{
 		phoneNumber, 94575, "a3406de8d171bb422bb6ddf3bbd800e2", &telegram.CodeSettings{},
 	})
-	// dry.PanicIfErr(err)
+	dry.PanicIfErr(err)
 	pp.Println(setCode)
 
 	fmt.Print("Auth code:")
